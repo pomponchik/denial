7a1f016945b70ee0b38871cf95ed09f16287654b
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-01-17 15:17:31 +0300

    Add type hints and support for optional ID in InnerNoneType

--- denial/inner.py
+++ denial/inner.py
@@ -1,28 +1,31 @@
 from itertools import count
+from typing import Any, Optional, Union
 
 from printo import descript_data_object
 
 
 class InnerNoneType:
     id: Optional[int]
-    counter = itertools.count()
+    counter = count()
 
-    def __new__(self) -> 'InnerNoneType':
-        self.id = next(self.counter)
-
-    def __init__(self, id: Union[int, str]) -> None:
-        self.id = id
+    def __init__(self, id: Optional[Union[int, str]] = None) -> None:  # noqa: A002
+        if id is None:
+            self.id = next(self.counter)
+        else:
+            self.id = id
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, type(self)):
             return False
         return self.id == other.id
 
+    def __hash__(self) -> int:
+        return hash(self.id)
+
     def __repr__(self) -> str:
         if not self.id:
             return 'InnerNone'
-        else:
-            return descript_data_object(type(self).__name__, (self.id,), {})
+        return descript_data_object(type(self).__name__, (self.id,), {})
 
 
 InnerNone = InnerNoneType()
