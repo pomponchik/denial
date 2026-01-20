301f911aff17a8d2212674f856f4f6c5525f1db0
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-01-20 19:25:00 +0300

    Add conditional TypeAlias import for Python < 3.10

--- denial/sentinel_type.py
+++ denial/sentinel_type.py
@@ -1,4 +1,10 @@
-from typing import TypeAlias, Optional
+import sys
+from typing import Optional
+
+if sys.version_info <= (3, 10):
+    from typing_extensions import TypeAlias  # pragma: no cover
+else:
+    from typing import TypeAlias
 
 from denial import InnerNoneType
 
