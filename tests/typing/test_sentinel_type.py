ad5a4d0c23da66578010eb66abee6f754953e4c3
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-01-20 19:14:03 +0300

    Add tests for Sentinel type compatibility with InnerNone and None

---
+++ tests/typing/test_sentinel_type.py
@@ -0,0 +1,23 @@
+import pytest
+
+from denial import InnerNone, InnerNoneType, Sentinel
+
+
+@pytest.mark.mypy_testing
+def test_sentinel_is_including_inner_none_and_just_none():
+    variable: Sentinel = InnerNone  # noqa: F841
+    another_variable: Sentinel = None  # noqa: F841
+
+
+@pytest.mark.mypy_testing
+def test_sentinel_is_including_inner_none_type():
+    variable: Sentinel = InnerNoneType()  # noqa: F841
+    another_variable: Sentinel = InnerNoneType(123)  # noqa: F841
+    another_else_variable: Sentinel = InnerNoneType('kek')  # noqa: F841
+
+
+@pytest.mark.mypy_testing
+def test_wrong_using_of_sentinel():
+    variable: Sentinel = 123 # E: Incompatible types in assignment (expression has type "int", variable has type "InnerNoneType | None")
+    another_variable: Sentinel = 'kek' # E: Incompatible types in assignment (expression has type "str", variable has type "InnerNoneType | None")
+    another_else_variable: Sentinel = False # E: Incompatible types in assignment (expression has type "bool", variable has type "InnerNoneType | None")
