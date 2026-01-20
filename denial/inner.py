fe4b604c2386dff4591f7fe60c3765bef77b06e7
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-01-20 20:43:03 +0300

    Add auto flag to InnerNoneType to distinguish auto-generated IDs

--- denial/inner.py
+++ denial/inner.py
@@ -6,18 +6,21 @@ from printo import descript_data_object
 
 class InnerNoneType:
     id: Optional[Union[int, str]]  # pragma: no cover
+    auto: bool  # pragma: no cover
     counter = count()
 
     def __init__(self, id: Optional[Union[int, str]] = None) -> None:  # noqa: A002
         if id is None:
             self.id = next(self.counter)
+            self.auto = True
         else:
             self.id = id
+            self.auto = False
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, type(self)):
             return False
-        return self.id == other.id
+        return self.id == other.id and self.auto == other.auto
 
     def __hash__(self) -> int:
         return hash(self.id)
