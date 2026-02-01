cc4b61c6f0f6e81bd731872f268e85195da8a74e
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-01 10:07:53 +0300

    Add doc parameter to InnerNoneType and update repr to include it

--- denial/inner.py
+++ denial/inner.py
@@ -1,7 +1,7 @@
 from itertools import count
 from typing import Any, Optional, Union
 
-from printo import descript_data_object
+from printo import descript_data_object, not_none
 
 
 class InnerNoneType:
@@ -9,13 +9,14 @@ class InnerNoneType:
     auto: bool  # pragma: no cover
     counter = count()
 
-    def __init__(self, id: Optional[Union[int, str]] = None, auto: bool = False) -> None:  # noqa: A002
+    def __init__(self, id: Optional[Union[int, str]] = None, doc: Optional[str] = None, auto: bool = False) -> None:  # noqa: A002
         if id is None:
             self.id = next(self.counter)
             self.auto = True
         else:
             self.id = id
             self.auto = auto
+        self.doc = doc
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, type(self)):
@@ -28,7 +29,7 @@ class InnerNoneType:
     def __repr__(self) -> str:
         if self.id == 0 and self.auto:
             return 'InnerNone'
-        return descript_data_object(type(self).__name__, (self.id,), {'auto': self.auto}, filters={'auto': lambda x: x != True})
+        return descript_data_object(type(self).__name__, (self.id,), {'auto': self.auto, 'doc': self.doc}, filters={'auto': lambda x: x != True, 'doc': not_none})
 
     def __bool__(self) -> bool:
         return False
